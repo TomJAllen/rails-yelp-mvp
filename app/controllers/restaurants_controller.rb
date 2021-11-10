7b57adddfecd8f0b6344d4535506048f26415073
Tom Allen <tomallen_1@hotmail.co.uk>
2021-11-10 12:31:43 +0000

    created restaurants controller as well as index and show pages

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,30 @@
+class RestaurantsController < ApplicationController
+
+before_action :get_restaurant, only: [:show]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  private
+
+  def get_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+end
+
+
+# A visitor can see the list of all restaurants.
+# GET "restaurants"
+# A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
+# GET "restaurants/new"
+# POST "restaurants"
+# A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
+# GET "restaurants/38"
+# A visitor can add a new review to a restaurant
+# GET "restaurants/38/reviews/new"
+# POST "restaurants/38/reviews"
