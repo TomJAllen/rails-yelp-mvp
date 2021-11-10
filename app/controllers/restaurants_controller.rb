3cf48ce69f20105bd2d09baa4a18b0d8acbfad13
Tom Allen <tomallen_1@hotmail.co.uk>
2021-11-10 13:01:43 +0000

    created new and create actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,15 +1,31 @@
 class RestaurantsController < ApplicationController
 
-before_action :get_restaurant, only: [:show]
+  before_action :get_restaurant, only: [:show]
 
   def index
     @restaurants = Restaurant.all
   end
 
-  def show
+  def show; end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
   end
 
   private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
 
   def get_restaurant
     @restaurant = Restaurant.find(params[:id])
